f5ca2809860b57a3eee8c6df3667a2ca6f364e20
SidLabs-Online <sidlabs.online@gmail.com>
2022-09-25 13:09:45 +0530

    updated test cases

--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
-from typing import List
+
 import unittest
 import _scanner
 
@@ -8,10 +8,10 @@ class TestIfList(unittest.TestCase):
     # Returns 'ok' if the scanner returns a list.
     def test_the_scanner(self):
 
-        self.assertEqual( type(_scanner.scan_now('8.8.8.8')), list)
-        self.assertEqual( type(_scanner.scan_now('127.0.0.0')), list)
-        self.assertEqual( type(_scanner.scan_now('20.220.195.124')), list)
-        self.assertEqual( type(_scanner.scan_now('8.8.8.8')), list)
+        self.assertEqual( type(_scanner.scan_now('8.8.8.8')), dict)
+        self.assertEqual( type(_scanner.scan_now('127.0.0.0')), dict)
+        self.assertEqual( type(_scanner.scan_now('20.220.195.124')), dict)
+        self.assertEqual( type(_scanner.scan_now('8.8.8.8')), dict)
 
 
 if __name__ == '__main__':

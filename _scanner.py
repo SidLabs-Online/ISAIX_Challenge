dec9d5c3ff3fdd9800e652455d9ea250ef377ae1
SidLabs-Online <sidlabs.online@gmail.com>
2022-10-04 21:38:19 +0530

    testing sockets ok

--- _scanner.py
+++ _scanner.py
@@ -1,32 +1,73 @@
-import nmap
-import json
 
-def scan_now(target):
-   #print(dir(nmap))
-    nScan = nmap.PortScanner()
+import socket
+
+
+tcpList = []
+sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def scan_now(target, ports):
+    landin_ip = socket.gethostbyname(target)
+
+    def scanner(ports):
+        try:
+            sk.connect(landin_ip, ports)
+            return True
+        except:
+            return False
+   
+    for items in ports:
+        if scanner(items):
+            tcpList.append([items, "open"])
+        else:
+            tcpList.append([items, "closed"])   
+
+    final_list = [target,[tcpList]]
+
+    return final_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import nmap
+# import json
+
+# def scan_now(target):
+#    #print(dir(nmap))
+#     nScan = nmap.PortScanner()
     
 
-    nScan.scan(target, '23,445,3389')
-    #nScan.scan(target, '22,25,80,111,443,3389')
-    tcpList = []
-    for host in nScan.all_hosts():
-        print('Host : %s (%s)' % (host, nScan[host].hostname()))
-        print('State : %s' % nScan[host].state())
-        s1 = nScan[host].state()
+#     nScan.scan(target, '23,445,3389')
+#     #nScan.scan(target, '22,25,80,111,443,3389')
+#     tcpList = []
+#     for host in nScan.all_hosts():
+#         print('Host : %s (%s)' % (host, nScan[host].hostname()))
+#         print('State : %s' % nScan[host].state())
+#         s1 = nScan[host].state()
         
-        for proto in nScan[host].all_protocols():
-            print('----------')
-            print('Protocol : %s' % proto)
-            lport = nScan[host][proto].keys()
-            #lport.sort()
-            for port in lport:
-                p1 = port
-                s1 =  nScan[host][proto][port]['state']
-                tcpList.append([p1, s1])
-                print ('port : %s\tstate : %s' % (port, nScan[host][proto][port]['state']))
+#         for proto in nScan[host].all_protocols():
+#             print('----------')
+#             print('Protocol : %s' % proto)
+#             lport = nScan[host][proto].keys()
+#             #lport.sort()
+#             for port in lport:
+#                 p1 = port
+#                 s1 =  nScan[host][proto][port]['state']
+#                 tcpList.append([p1, s1])
+#                 print ('port : %s\tstate : %s' % (port, nScan[host][proto][port]['state']))
             
-    final_list = [target,[tcpList]]
-    return final_list
+#     final_list = [target,[tcpList]]
+#     return final_list
     #print(final_list)
     
 
--- main.py
+++ main.py
@@ -3,23 +3,25 @@ import time
 begin = time.time()
 from colorama import Fore, Back, Style, init
 init()
-
+ports = [23,445,3389]
 the_target = input("Enter Target IP :")
 
-test = sn.scan_now(the_target)
+test = sn.scan_now(the_target, ports)
 
 end = time.time()
 print(Fore.BLUE + "\nScan completed in " + f"{round(((end - begin)%60), 2)}"+ " secs"+Style.RESET_ALL)
 
 
-print("\nVulnerability Threat Level\n")
-if test[1][0][1][1] == "open":
-    print("\t"+Back.RED + " high "+Style.RESET_ALL+Fore.RED ,f"RDP Server Detected overTCP."+ Style.RESET_ALL)
-elif test[1][0][0][1] == "open":
-    print("\t"+Back.MAGENTA + " critical "+Style.RESET_ALL+Fore.RED ,f"FTP Service Detected."+ Style.RESET_ALL)
-elif test[1][0][2][1] == "open":
-    print("\t"+Back.YELLOW + " medium "+Style.RESET_ALL+Fore.RED ,f"SMB Ports are Open Over TCP."+ Style.RESET_ALL)
-else:
-    print("\t\tNone\n")
+print(test)
+
+# print("\nVulnerability Threat Level\n")
+# if test[1][0][1][1] == "open":
+#     print("\t"+Back.RED + " high "+Style.RESET_ALL+Fore.RED ,f"RDP Server Detected overTCP."+ Style.RESET_ALL)
+# elif test[1][0][0][1] == "open":
+#     print("\t"+Back.MAGENTA + " critical "+Style.RESET_ALL+Fore.RED ,f"FTP Service Detected."+ Style.RESET_ALL)
+# elif test[1][0][2][1] == "open":
+#     print("\t"+Back.YELLOW + " medium "+Style.RESET_ALL+Fore.RED ,f"SMB Ports are Open Over TCP."+ Style.RESET_ALL)
+# else:
+#     print("\t\tNone\n")
 
 
